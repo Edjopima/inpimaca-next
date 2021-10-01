c5efbe818a2cff5877ddb88121f3c4b4e31361e8
Eduardo Piña <mrejpm@gmail.com>
2021-10-01 15:41:38 -0400

    :lipstick: improve header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,15 +3,21 @@ import styles from './Header.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
 import Logo from '../../public/images/logo.jpg'
-import shoppingCartIcon from '../../public/images/shopping-cart.svg'
+import ShoppingCartIcon from '../../public/images/shopping-cart.svg'
 
 const Header = () => {
   return (
     <header className={styles.header}>
+      <div></div>
       <div className={styles.logo}>
         <Image src={Logo} width={120} height={80} layout='responsive' className={styles.logo}/>
       </div>
-      <Image src={shoppingCartIcon} width={40} height={40} className={styles.shoppingCartIcon}/>
+      <Link href='/shoppingCart'>
+        <div className={styles.shoppingCart}>
+          <ShoppingCartIcon width={40} height={40} className={styles.shoppingCartIcon}/>
+          <p>Carrito (0)</p>
+        </div>
+      </Link>
     </header>
   );
 }
